9f94979feb35bd767c6ae9424647141c36662110
Ayushman Srivastava <ayushman1024@gmail.com>
2020-08-02 22:35:42 +0530

    corrected filedialog so that it points to root folder when opened; corrected report file name

--- src/pass1.py
+++ src/pass1.py
@@ -7,16 +7,16 @@ from tkinter import filedialog
 import filecmp
 
 now = datetime.now()
-timestamp = now.strftime("%Y%b%d_%H%M%S")
+timestamp = now.strftime("%d%b%Y%H%M%S")
 print(timestamp)
 
 gui = tk.Tk()
 gui.withdraw()
 
-root = filedialog.askdirectory(title="Folder to Analyze")
+root = filedialog.askdirectory(initialdir="/",title="Folder to Analyze")
 fname = root.split(os.path.sep)[-1]
 
-backup_pt_select = filedialog.askdirectory(title="Select Path Backup Folder")
+backup_pt_select = filedialog.askdirectory(initialdir="/",title="Select Path Backup Folder")
 backup = os.path.join(backup_pt_select, fname) + timestamp
 
 root_uq = os.path.join(backup, fname) + "_unique"
@@ -66,7 +66,11 @@ def getBookList(folder, base):
 def execute():
     getBookList(root, "") # recursively list all files found in "blist"
     books = pd.DataFrame(blist, columns=['filename', 'location', 'size'])
-    books.to_excel(fname + ".xlsx")
+    if len(fname)>20:
+        report_fname = fname[:20] +"_"+timestamp+ ".xlsx"
+    else:
+        report_fname = fname +"_"+timestamp+ ".xlsx"
+    books.to_excel(report_fname)
     print("Totals files ", len(blist))
 
     books_unq = books.drop_duplicates(subset='size', keep='first')
