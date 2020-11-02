f9f5728960dfdb31ec0f92f056588e031865be4e
Ayushman Srivastava <ayushman1024@gmail.com>
2020-11-02 22:48:28 +0530

    variable and function renaming

--- driver.py
+++ driver.py
@@ -1,5 +1,7 @@
 import src.pass1 as pass1
+import os
 
+os.walk()
 def start():
     pass1.execute()
 
--- src/pass1.py
+++ src/pass1.py
@@ -6,7 +6,6 @@ import tkinter as tk
 from tkinter import filedialog
 # import filecmp
 
-k = open("test.txt",'w+')
 now = datetime.now()
 timestamp = now.strftime("%d%b%Y%H%M%S")
 print(timestamp)
@@ -14,70 +13,74 @@ print(timestamp)
 gui = tk.Tk()
 gui.withdraw()
 
-root = filedialog.askdirectory(initialdir="/",title="Folder to Analyze")
+root = filedialog.askdirectory(initialdir="/", title="Folder to Analyze")
 fname = root.split(os.path.sep)[-1]
 
 backup_pt_select = filedialog.askdirectory(initialdir="/",title="Select Path Backup Folder")
-backup = os.path.join(backup_pt_select, fname) + timestamp
+backup_pt = os.path.join(backup_pt_select, fname) + timestamp
 
-root_uq = os.path.join(backup, fname) + "_unique"
-root_bk = os.path.join(backup, fname) + "_backup"
-blist = []
-bqlistpath = []
+root_uq = os.path.join(backup_pt, fname) + "_unique"
+root_bk = os.path.join(backup_pt, fname) + "_backup"
+allFiles_list = []
+unqPath_list = []
 
 def iterate(folder, base):
-    fdir = os.path.join(base, folder)
-    dirlist = os.listdir(fdir)
-    for f in dirlist:
-        fpath = os.path.join(fdir, f)
+    #pass2
+    file_dir = os.path.join(base, folder)
+    dir_list = os.listdir(file_dir) #ls
+    for f in dir_list:
+        fpath = os.path.join(file_dir, f)
         if os.path.isdir(fpath):
-            iterate(f, fdir)
+            iterate(f, file_dir)
         else:
-            if fpath in bqlistpath:
-                destF = os.path.join(root_uq, fdir.split(os.path.sep)[-1])
-                dest = os.path.join(root_uq, os.path.join(fdir.split(os.path.sep)[-1], f))
-                if not os.path.exists(destF):
-                    os.makedirs(destF)
+            if fpath in unqPath_list:
+                destination_path = os.path.join(root_uq, file_dir.split(os.path.sep)[-1])
+                dest = os.path.join(root_uq, os.path.join(file_dir.split(os.path.sep)[-1], f))
+                if not os.path.exists(destination_path):
+                    os.makedirs(destination_path)
                 copy2(fpath, os.path.join(root_uq, dest))
-                print("saved: " + os.path.join(root_uq, os.path.join(fdir.split(os.path.sep)[-1], f)))
+                print("saved: " + os.path.join(root_uq, os.path.join(file_dir.split(os.path.sep)[-1], f)))
 
             else:
-                destF = os.path.join(root_bk, fdir.split(os.path.sep)[-1])
-                dest = os.path.join(root_bk, os.path.join(fdir.split(os.path.sep)[-1], f))
-                if not os.path.exists(destF):
-                    os.makedirs(destF)
+                destination_path = os.path.join(root_bk, file_dir.split(os.path.sep)[-1])
+                dest = os.path.join(root_bk, os.path.join(file_dir.split(os.path.sep)[-1], f))
+                if not os.path.exists(destination_path):
+                    os.makedirs(destination_path)
                 copy2(fpath, dest)
                 os.remove(fpath)
                 print("removed: ", fpath)
 
-# def findDups(books: pd.DataFrame):
-#     books[books.duplicated(['size'])]
-#     pass
-
-def getBookList(folder, base):
+def generate_allFiles_list(folder, base):
+    #pass1
     fdir = os.path.join(base, folder)
     dirlist = os.listdir(fdir)
     for f in dirlist:
         fpath = os.path.join(fdir, f)
         if os.path.isdir(fpath):
-            getBookList(f, fdir)
+            generate_allFiles_list(f, fdir)
         else:
-            blist.append([f, fpath, os.stat(fpath).st_size])
+            allFiles_list.append([f, fpath, os.stat(fpath).st_size])
 
 def execute():
-    getBookList(root, "") # recursively list all files found in "blist"
-    books = pd.DataFrame(blist, columns=['filename', 'location', 'size'])
+    generate_allFiles_list(root, "") # recursively list all files found in "blist"
+
+    #unique finder
+    allFile_df = pd.DataFrame(allFiles_list, columns=['filename', 'location', 'size'])
+
+    unqFiles_df = allFile_df.drop_duplicates(subset='size', keep='first')
+    duplicate_count = allFile_df.shape[0] - unqFiles_df.shape[0]
+    print("Duplicates ", duplicate_count)
+
+    unqFiles_list = list(unqFiles_df.values)
+    for _ in unqFiles_list:
+        unqPath_list.append(_[1])
+    iterate(root, "")
+
+    #Report generation
     if len(fname)>20:
         report_fname = fname[:20] +"_"+timestamp+ ".xlsx"
     else:
         report_fname = fname +"_"+timestamp+ ".xlsx"
-    books.to_excel(report_fname)
-    print("Totals files ", len(blist))
 
-    books_unq = books.drop_duplicates(subset='size', keep='first')
-    print("Duplicates ", books.shape[0] - books_unq.shape[0])
-
-    bqlist = list(books_unq.values)
-    for bq in bqlist:
-        bqlistpath.append(bq[1])
-    iterate(root, "")
+    allFile_df.to_excel(report_fname)
+    print("Totals files ", len(allFiles_list))
